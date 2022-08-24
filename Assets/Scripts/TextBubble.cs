7df0b521760bab2f81a9f745da6e7c05c031aeb7
jkalnins81 <j_kalnins@hotmail.com>
2022-08-24 18:49:54 +0200

    TextBubble updates

--- Assets/Scripts/TextBubble.cs
+++ Assets/Scripts/TextBubble.cs
@@ -10,19 +10,78 @@ public class TextBubble : MonoBehaviour
     [SerializeField] GameObject textBubbleButton;
     [SerializeField] GameObject closeButton;
 
+    [SerializeField] DragAndDrop square;
+    [SerializeField] DragAndDrop square1;
+    [SerializeField] DragAndDrop square2;
+    [SerializeField] DragAndDrop square3;
+    [SerializeField] DragAndDrop square4;
+    [SerializeField] DragAndDrop square5;
+
+    private void OnEnable()
+    {
+        square.IsPlaced += ShowTextBubble;
+        square1.IsPlaced += ShowTextBubble1;
+        square2.IsPlaced += ShowTextBubble2;
+        square3.IsPlaced += ShowTextBubble3;
+        square4.IsPlaced += ShowTextBubble4;
+        square5.IsPlaced += ShowTextBubble5;
+    }
+    private void OnDisable()
+    {
+        square.IsPlaced -= ShowTextBubble;
+        square1.IsPlaced -= ShowTextBubble;
+        square2.IsPlaced -= ShowTextBubble;
+        square3.IsPlaced -= ShowTextBubble;
+        square4.IsPlaced -= ShowTextBubble;
+        square5.IsPlaced -= ShowTextBubble;
+    }
+
     void Start()
     {
         textBubbleButton.SetActive(false);
         closeButton.SetActive(false);
     }
 
-    private void OnMouseUp()
+    void ShowTextBubble()
     {
-        textBubbleButton.SetActive(false);
-        closeButton.SetActive(false);
+        ShowButton();
+        m_Object.text = "Insert Text Bubble text as appropriate";
     }
 
-    void ShowTextBubble()
+    void ShowTextBubble1()
+    {
+        ShowButton();
+        m_Object.text = "Insert Text Bubble text 1 as appropriate";
+
+    }
+
+    void ShowTextBubble2()
+    {
+        ShowButton();
+        m_Object.text = "Insert Text Bubble text 2 as appropriate";
+    }
+
+    void ShowTextBubble3()
+    {
+        ShowButton();
+        m_Object.text = "Insert Text Bubble text 3 as appropriate";
+    }
+
+    void ShowTextBubble4()
+    {
+        ShowButton();
+        m_Object.text = "Insert Text Bubble text 4 as appropriate";
+
+    }
+
+    void ShowTextBubble5()
+    {
+        ShowButton();
+        m_Object.text = "Insert Text Bubble text 5 as appropriate";
+
+    }
+
+    void ShowButton()
     {
         textBubbleButton.SetActive(true);
         closeButton.SetActive(true);
@@ -34,10 +93,10 @@ public class TextBubble : MonoBehaviour
         closeButton.SetActive(false);
     }
 
-    public void ClickTextBubble()
-    {
-        m_Object.text = "Insert text as appropriate";
-    }
+    //public void ClickTextBubble()
+    //{
+    //    m_Object.text = "Insert text as appropriate";
+    //}
 
     private void Update()
     {
