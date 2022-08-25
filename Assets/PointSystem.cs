5a1b4f79ece4a4dba783271ba8953e4d73d25926
jkalnins81 <j_kalnins@hotmail.com>
2022-08-25 11:14:56 +0200

    Update PointSystem.cs

--- Assets/PointSystem.cs
+++ Assets/PointSystem.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PointSystem : MonoBehaviour
 {
+    int TotalPoints;
+
     public int HatPoints;
     public int TopPoints;
     public int BottomPoints;
@@ -13,5 +15,20 @@ public class PointSystem : MonoBehaviour
         HatPoints = 0;
         TopPoints = 0;
         BottomPoints = 0;
+    }
+
+    public void CalculateTotalPoints()
+    {
+        TotalPoints = HatPoints + TopPoints + BottomPoints;
+
+        if(TotalPoints >= 2)
+        {
+            // load heaven scene
+        }
+
+        if(TotalPoints <= 1)
+        {
+            // load hell scene
+        }
     }
 }
